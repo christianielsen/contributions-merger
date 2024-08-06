acfd5c287630307b648b398b80caf5efd21f4a9b
Chris <chris.nielsen1226@gmail.com>
2024-08-06 21:54:35 +0100

    Add a toast message for empty username fields

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,33 @@
 <script setup>
 import { ref, computed } from "vue";
+import { useToast } from "primevue/usetoast";
 import ContributionsChart from "./components/ContributionsChart.vue";
 
 const username1 = ref(null);
 const username2 = ref(null);
-const renderGraph = ref(false);
+const shouldRenderGraph = ref(false);
+const toast = useToast();
 
 const canRenderGraph = computed(() => {
   return username1.value && username2.value;
 });
 
 const renderGraph = () => {
-  if (canRenderGraph) {
-    renderGraph.value = true;
+  if (canRenderGraph.value) {
+    shouldRenderGraph.value = true;
   } else {
+    toast.add({
+      severity: "warn",
+      summary: "Warning",
+      detail: "Please enter both usernames",
+      life: 3000,
+    });
   }
 };
 </script>
 
 <template>
+  <Toast />
   <div class="flex items-stretch p-5">
     <FloatLabel class="m-2">
       <InputText id="username1" type="text" v-model="username1" />
@@ -31,7 +40,7 @@ const renderGraph = () => {
     <Button class="m-2" @click="renderGraph()">Render Graphs</Button>
   </div>
   <ContributionsChart
-    v-if="canRenderGraph"
+    v-if="shouldRenderGraph"
     :username1="username1"
     :username2="username2"
   />
